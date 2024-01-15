e66507bae16c9afde4b3575d8ad60e5a5d804d4d
fsanchez334 <fernandosanchez955@gmail.com>
2024-01-15 00:02:12 -0500

    [fsanchez334] Added prompts for the user to input information

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import credit_card_calc as cc
 if __name__ == "__main__":
-    print("Hello there - provide inputs to gather your_reports")
-    chase_unlimited = ('Chase Freedom Unlimited' , 500, 3600)
-    chase_sapphire = ('Chase Sapphire', 470, 16000)
-    amex = ('Amex', 290, 5500)
-    best_buy = ('Best Buy', 0, 3000)
-
-    interest_rates = {
-        'Chase Freedom Unlimited': 0.2024,
-        'Chase Sapphire': 0.2349,
-        'Amex': 0.00,
-        'Best Buy': 0.2
-    }
-    cards = [chase_unlimited, chase_sapphire, amex, best_buy]
-    print(cc.credit_utilization(cards))
-    #Sorted list of credit cads to pay
-    print(cc.interest_rate(cards, interest_rates))
+    print("Hello there!")
+    print("We will compile your provided expenses and provide a report in return")
+    print("You will need to provide the name of your credit card, your current balance, your limit, and your APR")
+    
+    credit_cards = []
+    while True:
+        container = [x for x in input("Please provide name of bank, the balance you have on that card, the limit, and the APR: ").split(" ")]
+        if container[0] == '':
+            break
+        else:
+            for i in range(1, len(container)):
+                container[i] = float(container[i])
+            credit_card_tuple = tuple(container)
+            print(credit_card_tuple)
+            credit_cards.append(credit_card_tuple)
+    
+    
+    print(cc.credit_utilization(credit_cards))
