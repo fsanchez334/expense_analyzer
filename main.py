8b3efcca35c2117b3f56694a158eb6e86a01148f
fsanchez334 <fernandosanchez955@gmail.com>
2024-01-15 19:50:50 -0500

    [fsanchez334] Prompted user to provide take home pay

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import credit_card_calc as cc
 if __name__ == "__main__":
+    
+    
+
     print("Hello there!")
     print("We will compile your provided expenses and provide a report in return")
-    print("You will need to provide the name of your credit card, your current balance, your limit, and your APR")
+    pay = float(input("We will apply the 50-30-20 rule - please provide your pay per pay period: "))
+    print(cc.fifty_thirty_rule(pay))
+    # credit_cards = []
+    # interest_rates = {}
+    # while True:
+    #     container = [x for x in input("Please provide name of bank, the balance you have on that card, the limit, and the APR: ").split(" ")]
+    #     if container[0] == '':
+    #         break
+    #     else:
+    #         for i in range(1, len(container)):
+    #             container[i] = float(container[i])
+    #         credit_card_tuple = tuple(container)
+    #         print(credit_card_tuple)
+    #         credit_cards.append(credit_card_tuple)
+    #         interest_rates[credit_card_tuple[0]] = credit_card_tuple[3]
     
-    credit_cards = []
-    while True:
-        container = [x for x in input("Please provide name of bank, the balance you have on that card, the limit, and the APR: ").split(" ")]
-        if container[0] == '':
-            break
-        else:
-            for i in range(1, len(container)):
-                container[i] = float(container[i])
-            credit_card_tuple = tuple(container)
-            print(credit_card_tuple)
-            credit_cards.append(credit_card_tuple)
-    
-    
-    print(cc.credit_utilization(credit_cards))
+    # print(interest_rates)
+    # print(cc.credit_utiliza
+    # tion(credit_cards))
+    # print(cc.calculate_accrued_interest())
