f76d68384058e7847e454106ec0e07f36c527079
fsanchez334 <fernandosanchez955@gmail.com>
2024-01-24 23:03:46 -0500

    [fsanchez334] Simplified the main program to only rely on an ExpenseAnalyzer object

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import credit_card_calc as cc
+from expense_functions import ExpenseAnalyzer
+import pandas as pd
 
 if __name__ == "__main__":
     
@@ -7,5 +8,10 @@ if __name__ == "__main__":
     print("Hello there!")
     print("We will compile your provided expenses and provide a report in return")
     pay = float(input("We will apply the 50-30-20 rule - please provide your check pay: "))
-    tuplet_split = cc.fifty_thirty_rule(pay)
-    print("50% is {}, 30 % is {}, and 20% is {}".format(tuplet_split[0], tuplet_split[1], tuplet_split[2]))
+    #TODO: Probably give context to what each percentage represents
+
+    EA = ExpenseAnalyzer(pay, "sample_expenses.csv")
+    EA.provideUserStatus()
+    total_expenses = EA.expense_collection()
+    print("The total amount of expenses you need to cover is {}".format(total_expenses))
+    
