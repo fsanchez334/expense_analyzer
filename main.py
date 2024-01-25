8e877cf6b9c83a56a99dbcb2269c7e543f2780dd
fsanchez334 <fernandosanchez955@gmail.com>
2024-01-24 22:13:47 -0500

    [fsanchez334] Adding more prompts for the user

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import credit_card_calc as cc
+
 if __name__ == "__main__":
     
     
 
     print("Hello there!")
     print("We will compile your provided expenses and provide a report in return")
-    pay = float(input("We will apply the 50-30-20 rule - please provide your pay per pay period: "))
-    print(cc.fifty_thirty_rule(pay))
+    pay = float(input("We will apply the 50-30-20 rule - please provide your check pay: "))
+    tuplet_split = cc.fifty_thirty_rule(pay)
+    print("50% is {}, 30 % is {}, and 20% is {}".format(tuplet_split[0], tuplet_split[1], tuplet_split[2]))
     # credit_cards = []
     # interest_rates = {}
     # while True:
