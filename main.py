cd395da38b60c4ea4a66c8b90b7f7191096ec6a5
fsanchez334 <fernandosanchez955@gmail.com>
2024-01-27 11:05:40 -0500

    [fsanchez334] Added some more print statement and tried to make it more readable

--- main.py
+++ main.py
@@ -5,13 +5,23 @@ if __name__ == "__main__":
     
     
 
-    print("Hello there!")
-    print("We will compile your provided expenses and provide a report in return")
-    pay = float(input("We will apply the 50-30-20 rule - please provide your check pay: "))
-    #TODO: Probably give context to what each percentage represents
+    print("="*90)
+    print("Expense Analyzer")
+    print("=" * 90)
+    print("The 50-30-20 rule")
+    print("=" * 90)
+
+    print("We will compile your provided expenses and provide a report in return using the 50-30-20 rule")
+    print("The idea is to divide your income into three categories, spending 50% on needs, 30% on wants, and 20% on savings")
+    print("=" * 90)
+    pay = float(input("Please provide your check pay: "))
 
     EA = ExpenseAnalyzer(pay, "sample_expenses.csv")
     EA.provideUserStatus()
-    total_expenses = EA.expense_collection()
-    print("The total amount of expenses you need to cover is {}".format(total_expenses))
-    
+    total_expenses = EA.getAmountforNeeds()
+    if pay >= total_expenses:
+        print("Paying your expenses result in the following:")
+        #Assumes the user can cover the expenses
+        EA.updateNeeds(-1 * total_expenses)
+        EA.updateTotal(-1 * total_expenses)
+        EA.provideUserStatus()
