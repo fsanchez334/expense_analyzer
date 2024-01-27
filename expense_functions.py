65d0f14781e9739fc9fdb9af8b0c09359394861d
fsanchez334 <fernandosanchez955@gmail.com>
2024-01-27 11:05:12 -0500

    [fsanchez334] Modified the names of the functions, added another varaible to track the total

--- expense_functions.py
+++ expense_functions.py
@@ -2,9 +2,10 @@ import pandas as pd
 
 class ExpenseAnalyzer:
     def __init__(self, paycheck_amount, expenses_file):
+        self.total_amount = paycheck_amount
         self.needs = paycheck_amount * 0.5
-        self.wants = paycheck_amount * 0.2
-        self.savings = paycheck_amount * 0.3
+        self.wants = paycheck_amount * 0.3
+        self.savings = paycheck_amount * 0.2
         self.expenses = pd.read_csv(expenses_file)
 
     def getSavings(self):
@@ -16,13 +17,16 @@ class ExpenseAnalyzer:
     def getAmoutforWants(self):
         return self.wants
     
-    def modifySavingsAmount(self, amount):
+    def updateTotal(self, amount):
+        self.total_amount += amount
+    
+    def updateSavings(self, amount):
         self.savings += amount
     
-    def modifyNeedsAmount(self, amount):
+    def updateNeeds(self, amount):
         self.needs += amount
     
-    def modifyWantsAmount(self, amount):
+    def updateWants(self, amount):
         self.wants += amount
 
     def expense_collection(self):
@@ -31,6 +35,9 @@ class ExpenseAnalyzer:
         return sum(costs)
     
     def provideUserStatus(self):
+        print("*" * 90)
+        print("You currently have {}".format(self.total_amount))
         print("To cover must-have costs, you have {}".format(self.needs))
         print("The amount you can use for your savings is {}".format(self.savings))
         print("The amount you can use for your wants is {}".format(self.wants))
+        print("*" * 90)
