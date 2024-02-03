03073324ffa2ee2c3ef030242ae9dbd507aaa143
fsanchez334 <fernandosanchez955@gmail.com>
2024-02-02 22:45:22 -0500

    [fsanchez334] Moved the functions pertaining to credit to another file

--- expense_functions.py
+++ expense_functions.py
@@ -1,15 +1,10 @@
-from IPython.display import display
-import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-import heapq as hq
-
 class ExpenseAnalyzer:
-    def __init__(self, paycheck_amount, expenses_file):
+    def __init__(self, paycheck_amount, df):
         self.total_amount = paycheck_amount
         self.needs = paycheck_amount * 0.5
         self.wants = paycheck_amount * 0.3
         self.savings = paycheck_amount * 0.2
-        self.expenses = pd.read_csv(expenses_file)
+        self.expenses = df
 
     def getSavings(self):
         return self.savings
@@ -44,44 +39,6 @@ class ExpenseAnalyzer:
         print("The amount you can use for your wants is {}".format(self.wants))
         print("*" * 90)
 
-    def credit_card_utilization(self):
-        #Get Credit Card dataframes
-        credit_boolean = self.expenses["Credit Card?"] == "Y"
-        df_credit_card = self.expenses[credit_boolean]
-        total_credit_allowed = sum(df_credit_card.get("Total").tolist())
-        total_credit_used = sum(df_credit_card.get("Amount").tolist())
-        return float(total_credit_used/ total_credit_allowed)
-    
-    
-    def credit_card_breakdown(self):
-        #Here, we return the breakdown of each credit card, including amount used, total and interest rate
-        credit_boolean = self.expenses["Credit Card?"] == "Y"
-        df_credit_card = self.expenses[credit_boolean]
-        col = df_credit_card.apply(lambda row: float(row.Amount / row.Total), axis=1)
-        interest_column = df_credit_card.apply(lambda row: ((row["Interest Rate"] / 12) / 100) * row['Amount'], axis=1)
-
-        df_credit_card = df_credit_card.assign(percent_used=col.values)
-        df_credit_card = df_credit_card.assign(approximateInterest=interest_column)
-        columns = ["Expense", "Amount", "Total", "Interest Rate", "approximateInterest", "percent_used"]
-        df_credit_card = df_credit_card[columns]
-        return df_credit_card
-
-    def credit_heap(self, df):
-        #Here, we will take in the credit df and only get the CC, the amount, interest_rate, percent_used
-        focused_columns = ["Expense", "approximateInterest", "percent_used"]
-        df_starter = df[focused_columns]
-        #We will need to scale the interest and the percent used
-        scaler = MinMaxScaler()
-        df_starter[["approximateInterest", "percent_used"]] = scaler.fit_transform(df_starter[["approximateInterest", "percent_used"]])
-        #Now that we have constructed this - we are ready construct our Priority Heap (we can use the average of the scaled percentages)
-        credit_cards = df_starter["Expense"].tolist()
-        summation = ((df_starter["approximateInterest"] +  df_starter["percent_used"]) / 2).tolist()
-        heap = []
-        for i in range(len(credit_cards)):
-            heap.append((summation[i] * -1, credit_cards[i]))
-        hq.heapify(heap)
-        while len(heap) != 0:
-            print(hq.heappop(heap))
-            
+ 
         
         
