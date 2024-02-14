400d985d2ee39b3355262773d29d4b0f55e138cc
fsanchez334 <fernandosanchez955@gmail.com>
2024-02-13 21:34:34 -0500

    Added logic to take into consideration the due date of when payments are due

--- credit_analyzer.py
+++ credit_analyzer.py
@@ -2,6 +2,7 @@ from IPython.display import display
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 import heapq as hq
+from datetime import datetime
 
 class CreditAnalyzer:
     def __init__(self, credit_card_df):
@@ -25,7 +26,7 @@ class CreditAnalyzer:
 
         df_credit_card = self.credit_card_df.assign(percent_used=percent_column)
         df_credit_card = df_credit_card.assign(approximateInterest=interest_column)
-        columns = ["Expense", "Amount", "Total", "Interest Rate", "approximateInterest", "percent_used"]
+        columns = ["Expense", "Amount", "Total", "Interest Rate", "approximateInterest", "percent_used", "Date Due"]
         df_credit_card = df_credit_card[columns]
         return df_credit_card
     
@@ -38,22 +39,29 @@ class CreditAnalyzer:
 
     def credit_heap(self, df, credit_dict):
         #Here, we will take in the credit df and only get the CC, the amount, interest_rate, percent_used
-        focused_columns = ["Expense", "approximateInterest", "percent_used"]
+        focused_columns = ["Expense", "approximateInterest", "percent_used", "Date Due"]
         df_starter = df[focused_columns].copy()
+        #Now here, we need to add the column to that has the date due
+        df_starter["Current_date"] = pd.to_datetime(datetime.now())
+        df_starter["Due_Date"] = pd.to_datetime(df_starter["Date Due"])
+        df_starter["days_remaining"] = (df_starter["Due_Date"] - df_starter["Current_date"]).dt.days
         #We will need to scale the interest and the percent used
         scaler = MinMaxScaler()
         df_starter.loc[:, ["approximateInterest", "percent_used"]] = scaler.fit_transform(df_starter[["approximateInterest", "percent_used"]])
         #Now that we have constructed this - we are ready construct our Priority Heap (we can use the average of the scaled percentages)
         credit_cards = df_starter["Expense"].tolist()
-        summation = ((df_starter["approximateInterest"] +  df_starter["percent_used"]) / 2).tolist()
+        summation = (((df_starter["approximateInterest"] +  df_starter["percent_used"]) / 2) + df_starter["days_remaining"]).tolist()
         heap = []
         for i in range(len(credit_cards)):
-            heap.append((summation[i] * -1, credit_cards[i]))
+            name_modified = credit_cards[i].strip()
+            heap.append((summation[i], name_modified, credit_dict[name_modified]["Amount"], credit_dict[name_modified]["Date Due"]))
         hq.heapify(heap)
         while len(heap) != 0:
             credit_tuplet = hq.heappop(heap)
             credit_card_name = credit_tuplet[1]
-            print(credit_card_name)
+            credit_card_amount = credit_tuplet[2]
+            credit_card_due_date = credit_tuplet[3]
+            print("{} with amount {} with a due date of {}".format(credit_card_name, credit_card_amount, credit_card_due_date))
                 
             
     def calculateOptimalPayment(self, credit_dict, card_name, goal_utilization):
